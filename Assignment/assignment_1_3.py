949644b77f29d744c7336b8bdbe46897928cf8b6
mildChanadda <chanadda.r@rmutsvmail.com>
2022-08-24 16:09:50 +0700

    Initial commit

---
+++ Assignment/assignment_1_3.py
@@ -0,0 +1,30 @@
+"""
+Name: {Chanadda Rakkhaphan}
+SID: {364211760005}
+Group: {MIT221}
+"""
+
+"""
+เขียนโปรแกรมไพธอนเพื่อนำเลขบัตรประชาชนมาบวกกันเพื่อทำนายดวงชะตาดังนี้
+• สร้างฟังก์ชันเพื่อหาผลรวมของเลขบัตรประชาชน โดยใช้ชื่อฟังก์ชั่นว่า sumPID()
+• สร้างฟังชั่นชื่อ getFortune() เพื่อทำนายดวงชะตา ถ้าเป็นเลขคู่ให้ทำนายว่า your fortune is good
+• ถ้าเป็นเลขคี่ ให้ทำนายว่า your fortune is very good
+
+ใช้ฟังก์ชั่น split()
+"""
+
+def getPID():
+    pid = input('Enter your PID : ') #str
+    lpid = [int(x) for x in pid]
+    return lpid #List
+
+def getFortune(var):
+        if var % 2 == 0:
+            print('Your fortune is good')
+        else:
+            print('Your fortune is very good')
+
+
+PID = getPID()
+print(f'SumID card : {sum(PID)}')
+print(f'Prophecy : {getFortune(sum(PID))}')
